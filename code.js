60194007446e1b1ccda4b6e196b0e42a1383ebf7
AnneMarie Torresen <torresen@umich.edu>
2025-02-02 22:39:59 -0500

    Styling improvements and scroll bar

--- code.js
+++ code.js
@@ -3,24 +3,31 @@ var n = 0;
 function start() {
   n = document.getElementById("sonnetNumber").value - 1;
   document.getElementById("sonnet").innerHTML = sonnets[n];
-  document.getElementById("word").innerHTML = "<br>";
   buttons();
 }
 
 function buttons() {
+  // create a button for each possible avion of the chosen sonnet
   var buttons = [];
   for(let i = 0; i < (words[n]).length; i++) {
     var w = words[n][i];
     buttons.push(
-      "<button onclick='show(&quot;" + w + "&quot;," + i + ")'>" + w + "</button>"
+      "<tr><td><button id = " + i + " onclick='show(" + i + ")'>" + w + "</button></td></tr>"
     )
   };
+  buttons = "<div class='scroll'><table>" + buttons.join("") + "</table><div>"
   document.getElementById("wordList").innerHTML = buttons;
 }
 
-function show(w, i) {
+function show(i) {
+  // color selected button
+  for( b of [...document.getElementsByTagName("button")] ){
+    b.style.color = "gray"
+  };
+  document.getElementById(i + "").style.color = "black";
+
+  // color avion letters in sonnet text
   sonnet = sonnets[n].split("");
-  document.getElementById("word").innerHTML = w;
   for (j of indexes[n][i]) {
     sonnet[j] = "<em>" + (sonnet[j]).toUpperCase() + "</em>";
   };
